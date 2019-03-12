6a13bf5da522ac4f5ef56ac6c916418b5cc9fe88
Sixian Lin <davidlin006811@gmail.com>
2019-03-12 11:43:43 -0400

    Update mail.js

--- functions/mail.js
+++ functions/mail.js
@@ -1,20 +1,18 @@
 var nodemailer = require("nodemailer");
 
 module.exports = function(email_address, subject, content) {
-  var smtpTransporter = nodemailer.createTransport({
-    service: "Gmail",
+   var smtpTransporter = nodemailer.createTransport({
+    host: "smtp.zoho.com",
+    port: 465,
+    secure: true, // use SSL
     auth: {
-      type: "OAuth2",
-      user: "davidlin006811@gmail.com",
-      clientId:
-        "755175043144-2d0k6os5gqiargohl9c6i5th3m38dplu.apps.googleusercontent.com",
-      clientSecret: "kAGDuO-sG1lblAEZLXK6aZHF",
-      refreshToken: "1/AVIv9pXT27jWXw1JCsUe16U0oUPoSIMM9R_J1sA0dlQ"
+      user: "ironratcustoms@zoho.com",
+      pass: "RmMUyEHtjz2HBV:"
     }
   });
 
   var mailOptions = {
-    from: "Iron Rats Custom<davidlin006811@gmail.com>",
+    from: "Sixian Lin <ironratcustoms@zoho.com>",
     to: email_address,
     subject: subject,
     html: content
