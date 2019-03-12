1eaf45b24758bbe65b8a345be237f782b621ccd6
Sixian Lin <davidlin006811@gmail.com>
2019-03-12 15:56:40 -0400

    Delete appointment.js

--- models/appointment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const AppointmentSchema = new Schema({
-  name: String,
-  email: String,
-  appointment_date: Date
-});
-module.exports = mongoose.model("Appointment", AppointmentSchema);
