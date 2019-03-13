8dd55e1385676cab86cdf7ff3422f29a503ff524
zjw <zjw.working@gmail.com>
2019-03-12 22:00:08 -0400

    review router post to be test

---
+++ routers/post-test-example.js
@@ -0,0 +1,18 @@
+//message
+const message = 
+{
+  "_id": 0001,
+  "name": "Tester-004",
+  "email": "tester@gmail.com",
+  "content": "This is a test message"
+}
+
+
+// review
+
+const review = 
+{
+  "name": "Tester-004",
+  "rating": 3,
+  "comment": "Revew test text"
+}
--- routers/reviewRouter.js
+++ routers/reviewRouter.js
@@ -3,30 +3,51 @@ const router = express.Router();
 const Review = require("../models/review");
 
 //test
-router.get('/', (req, res) => {
-  res.send('Review router works!');
-});
+// router.get('/', (req, res) => {
+//   res.send('Review router works!');
+// });
 
 
 
 //1) Get all reviews API - /api/review/all, for the response, please refer to the client-side coding tasks.
 
-// router.get("/all", (req, res) => {
-//   Review.find()
-//     .sort({ date: -1 })
-//     .then(reviews => res.json(reviews));
-// });
+router.get("/all", (req, res) => {
+  Review.find()
+    .sort({ date: -1 })
+    .then(reviews => res.json(reviews));
+});
 
 //2) Post a review - /api/review, for the response, please refer to the client-side coding tasks.
 
-const current_date = new Date();
-
 router.post("/", (req, res) => {
-  const review = {
+  const review = new Review ({
     name: req.body.name,
-    date: current_date
-  };
-  res.json(review);
+    rating: req.body.rating,
+    comment: req.body.comment
+  });
+
+  review
+    .save()
+    .then(result => {
+      //console.log(result);
+      const newReview = {
+        createdReview: {
+          name: result.name,
+          rating: result.rating,
+          comment: result.comment
+        }
+      };
+      io.sockets.emit("new review", newReview);
+      res.status(201).json({
+        message: "submit a review successfully",
+        review
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 // export router
