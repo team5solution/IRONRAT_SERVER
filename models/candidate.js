9180213367a5399dcc8f9afa66b75c2e8743bfdc
Sixian Lin <davidlin006811@gmail.com>
2019-03-21 21:39:48 -0400

    change

--- models/candidate.js
+++ models/candidate.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const CandidateSchema = new Schema({
   name: String,
   email: String,
-  resume: [String]
+  resume: [String],
+  jobId: Schema.ObjectId
 });
 module.exports = mongoose.model("Candidate", CandidateSchema);
--- models/career.js
+++ models/career.js
@@ -1,16 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-//const Candidate = require("./candidate");
-const Candidate = new Schema({
-  name: String,
-  email: String,
-  resume: [String]
-});
+
 const CareerSchema = new Schema({
   title: String,
   description: String,
   images: [String]
-  //candidates: [Candidate]
 });
 
 module.exports = mongoose.model("Career", CareerSchema);
