4acb9864a875b9cbc8b942858bd727af143df364
Sixian Lin <davidlin006811@gmail.com>
2019-03-27 22:23:31 -0400

    modify user router

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,6 @@
 module.exports = {
   email: "lin00132@algonquinlive.com",
+  phone: "8192105712",
   SMSNumber: "8192105712@msg.koodomobile.com",
   tokenSecret: "IronRatsCustomOttawa",
   role: "admin"
